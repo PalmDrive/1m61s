5d7d9e97ac019eefbe9de2356e8991ab981ffa3c
jml <ljm@a.cc>
2016-12-08 17:10:24 +0800

    commit

---
+++ test/test_luckymoney.js
@@ -0,0 +1,13 @@
+var common = require('../lib/wechat_pay.js');
+var path = require("path")
+
+
+var _data = {
+    "re_openid":'oslYRxAmiWdvmXHs3p11gTJS0RC4',
+    "total_amount":100
+  },
+  _callback = ret => {
+    console.log("return:",ret);
+  };
+
+common.fnSendMoney(_data,_callback);
