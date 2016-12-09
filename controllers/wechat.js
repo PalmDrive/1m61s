852a1aadd4c90ac5675ec5fbee777f157ef96eec
x33tkai <1027779283@qq.com>
2016-12-09 14:30:52 +0800

    fix bug

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -19,7 +19,7 @@ const getTime = (startedAt) => {
 };
 
 const sendModelMessage = (accessToken) => {
-  logger.info(`sendModelMessage-- start`);
+  logger.info('sendModelMessage-- start');
   const data = {
     touser: 'oXrsBv-Gl6tjcwTIlCCqQzEAYoWg',
     money: '100',
