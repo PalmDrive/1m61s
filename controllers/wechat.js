e255b626612507713fdf70f3b4b650f86edccb80
x33tkai <1027779283@qq.com>
2016-12-09 14:07:40 +0800

    add log

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -19,7 +19,7 @@ const getTime = (startedAt) => {
 };
 
 const sendModelMessage = (accessToken) => {
-
+  console.log(`sendModelMessage-- start`);
   const data = {
     touser: 'oXrsBv-Gl6tjcwTIlCCqQzEAYoWg',
     money: '100',
@@ -37,6 +37,7 @@ const sendModelMessage = (accessToken) => {
     ]
   };
 
+  console.log(`sendModelMessage-- data:${data}`);
   request.post({
     url: `https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=${accessToken}`,
     json: true,
@@ -65,9 +66,11 @@ const sendModelMessage = (accessToken) => {
       }
     }
   }, (error, response, body) => {
-    if (error) return reject(error);
     console.log(`sendModelMessage--response: ${response}`);
     console.log(`sendModelMessage--body: ${body}`);
+    console.log(`sendModelMessage--err: ${error}`);
+    if (error) return reject(error);
+    
   });
   
 };
