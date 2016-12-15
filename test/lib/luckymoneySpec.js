961850102f3dfffe10c169a028d7ec9743c20804
jml <ljm@a.cc>
2016-12-15 16:38:38 +0800

    add test file

--- test/lib/luckymoneySpec.js
+++ test/lib/luckymoneySpec.js
@@ -4,19 +4,26 @@ const chai = require('chai'),
   should = chai.should(),
   Datetime = require('../../lib/datetime'),
   wechatPay = require('../../lib/wechat_pay.js'),
-  path = require("path");
+  child_process = require('child_process'),
+  //test_pay = require('../test_luckymoney.js'),
+  logger = require('../../lib/logger.js'),
+  path = require('path');
 
 describe('--test WechatPay library--', () => {
   it('send pomz007 1 yuan ', () => {
+    logger.log("use `node ../../test_luckmoney.js`")
+    //var _data = {
+    //    "re_openid": 'oslYRxAmiWdvmXHs3p11gTJS0RC4', //POMZ007's openid
+    //    "total_amount": 100 //cent
+    //  },
+    //  _callback = ret => {
+    //    console.log("returned:", ret);
+    //  };
+    //
+    //wechatPay.sendMoney(_data, _callback);
+    let _path = path.resolve(`${__filename}/../../test_luckymoney.js`)
+    child_process.execSync(`node ${_path}`)
 
-    var _data = {
-        "re_openid": 'oslYRxAmiWdvmXHs3p11gTJS0RC4', //POMZ007's openid
-        "total_amount": 100 //cent
-      },
-      _callback = ret => {
-        console.log("returned:", ret);
-      };
-    wechatPay.sendMoney(_data, _callback);
   });
 });
 
