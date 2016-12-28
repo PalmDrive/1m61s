75ade0a6947f90714a9428876b85ee2a07676ee2
x33tkai <1027779283@qq.com>
2016-12-28 17:53:44 +0800

    修改模板消息详情页面的样式 增加排名页面 增加奖励规则页面

--- 1m61_web/web.js
+++ 1m61_web/web.js
@@ -3,7 +3,11 @@
 const express = require('express'),
       _ = require('underscore'),
       wechat_pay = require('../lib/wechat_pay'),
-      wechat = require('../lib/wechat');
+      wechat = require('../lib/wechat'),
+      leanCloud = require('../lib/lean_cloud'),
+      LeanCloud = leanCloud.AV,
+      openId = '',
+      date;
 
 let app = express();
 
@@ -28,28 +32,33 @@ app.post('/pay', function(req, res, next) {
 
 app.get('/detailTask', function (req, res, next) {
   // 测试数据
-  // const content = [{title:'一',color:'1'},{title:'个'},{title:'测'},{title:'一',color:'1'},{title:'个',color:'0'},{title:'测'}];
-  // const data = [
-  //           {
-  //             content1: content,//'一个测试片段，逗号之后的另一段',
-  //             content2: '一个好的测试片段',
-  //             wrongWordsAmount: '8',
-  //             audioURL: 'http://ailingual-production.oss-cn-shanghai.aliyuncs.com/media_fragments/bb893acd-a15c-4eac-b928-7477b977df56/pchunk-00000109.wav'
-  //           },
-  //           {
-  //             content1: content,
-  //             content2: '一个好的测试片段',
-  //             wrongWordsAmount: '2',
-  //             audioURL: 'http://ailingual-production.oss-cn-shanghai.aliyuncs.com/media_fragments/bb893acd-a15c-4eac-b928-7477b977df56/pchunk-00000109.wav'
-  //           }
-  //         ];
+  const content = [{title:'一',color:'1'},{title:'个'},{title:'测'},{title:'一',color:'1'},{title:'个',color:'0'},{title:'测'}];
+  const data = {
+          errorTask: [
+            {
+              content1: content,//'一个测试片段，逗号之后的另一段',
+              content2: content,
+              wrongWordsAmount: '8',
+              audioURL: 'http://ailingual-production.oss-cn-shanghai.aliyuncs.com/media_fragments/bb893acd-a15c-4eac-b928-7477b977df56/pchunk-00000109.wav'
+            },
+            {
+              content1: content,
+              content2: content,
+              wrongWordsAmount: '2',
+              audioURL: 'http://ailingual-production.oss-cn-shanghai.aliyuncs.com/media_fragments/bb893acd-a15c-4eac-b928-7477b977df56/pchunk-00000109.wav'
+            }
+          ],
+          reward: '9元',
+          rewardRate: '80%'
+        };
 
-  // res.render('detailTask',{data: data, date: '2016/12/09'});
-  // next();
+  res.render('detailTask',{data: data, date: '2016/12/09'});
+  next();
 
-  const date2 = new Date(req.query['date2']);
-  wechat.getUserTaskData(req.query['openId'], new Date(req.query['date1']), date2).then(data => {
-    res.render('detailTask',{data: data, date: date2.toLocaleDateString()});
+  date = new Date(req.query['date2'].substr(0,10));
+  openId = req.query['openId'];
+  wechat.getUserTaskData(openId, new Date(req.query['date1']), new Date(req.query['date2'])).then(data => {
+    res.render('detailTask',{data});
     next();
   }).catch(Exception => {
      console.log('Exception:' + JSON.stringify(Exception));
@@ -57,4 +66,53 @@ app.get('/detailTask', function (req, res, next) {
   
 });
 
-module.exports.app =app;
+app.get('/rules', function (req, res, next) {
+  res.render('rules', {} );
+  next();
+});
+
+app.get('/ranking', function (req, res, next) {
+
+  let promiseArray = [],
+      data = {},
+      ranking = {};
+  const rightRatePromise = promiseQuery('right_task_rate').then(results => {
+    data.rightRateList = results.splice(0,10);
+  });
+  const todayTaskPromise = promiseQuery('task_amount').then(results => {
+    for (var i = 0; i < results.length; i++) {
+      const res = results[i].toJSON();
+      if(res.open_id === openId) {
+        data.ranking = i + 1;
+        data.task_amount = res.task_amount;
+        data.total_task_amount = res.total_task_amount;
+        break;
+      }
+    }
+    data.todayTaskList = results.splice(0,10);
+  });
+  const totalTaskPromise = promiseQuery('total_task_amount').then(results => {
+    data.totalTaskList = results.splice(0,10);
+  });
+  promiseArray.push(rightRatePromise);
+  promiseArray.push(todayTaskPromise);
+  promiseArray.push(totalTaskPromise);
+
+  Promise.all(promiseArray).then(results => {
+    res.render('ranking', {data} );
+    next();
+  });
+});
+
+const promiseQuery = (ranking) => {
+  const query = new LeanCloud.Query("WeChatUser");
+  query.greaterThanOrEqualTo('updatedAt', date);
+  query.lessThanOrEqualTo('updatedAt', date);
+  query.descending(ranking);
+  // query.limit(10);
+  return query.find().then(function(results) {
+    return results;
+  });
+};
+
+module.exports.app = app;
