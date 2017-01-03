96fbbdeed9f35be0d3ee88903e000d8091758819
Brody Chen <rufengch@gmail.com>
2017-01-03 15:19:33 -0800

    Fix: status

--- controllers/wechat.js
+++ controllers/wechat.js
@@ -1272,7 +1272,7 @@ const onReceivePass = (data, accessToken, task, user) => {
 };
 
 // Return user object
-const ruleTeaching = (data, accessToken, user, currentTaskOrder, delay) => {
+const ruleTeaching = (data, accessToken, user, currentTaskOrder, status, delay) => {
   const skillGotArray = [9, 11, 14, 17, 20, 23],
         ruleArray = [9, 11, 17, 20],
         nextTaskOrder = currentTaskOrder + 1,
@@ -1516,7 +1516,7 @@ const onReceiveFromB = (data, accessToken, user) => {
         content += `【红包奖励：${redPacket}/8元】\n\n恭喜你，你的答案是正确的！`;
         sendToUser.text(content, data, accessToken);
 
-        user = ruleTeaching(data, accessToken, user, currentTaskOrder);
+        user = ruleTeaching(data, accessToken, user, currentTaskOrder, status);
       }
       user.save();
     }
@@ -1569,7 +1569,7 @@ const onReceiveFromB = (data, accessToken, user) => {
                 sendToUser.text(content, data, accessToken);
               }, 1000);
 
-              user = ruleTeaching(data, accessToken, user, currentTaskOrder, 1000);
+              user = ruleTeaching(data, accessToken, user, currentTaskOrder, status, 1000);
 
               // Update WeChatUser
               user.save();
