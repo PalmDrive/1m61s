959562ef6c69423b69cba0a64671c5208a9dfe0c
x33tkai <1027779283@qq.com>
2017-01-05 10:34:18 +0800

    code style

--- static/app.js
+++ static/app.js
@@ -1,41 +1,41 @@
 $(function(){
 
-  $(".rightRate").show();
-  $(".todayTask").hide();
-  $(".totalTask").hide();
+  $('.rightRate').show();
+  $('.todayTask').hide();
+  $('.totalTask').hide();
 
-  $(".todayTaskClick").css("color","#000000");
-  $(".rightRateClick").css("color","#0000ff");
-  $(".totalTaskClick").css("color","#000000");
+  $('.todayTaskClick').css('color','#000000');
+  $('.rightRateClick').css('color','#0000ff');
+  $('.totalTaskClick').css('color','#000000');
 
    window.todayTaskClick = function() {
-    $(".rightRate").hide();
-    $(".todayTask").show();
-    $(".totalTask").hide();
+    $('.rightRate').hide();
+    $('.todayTask').show();
+    $('.totalTask').hide();
 
-    $(".todayTaskClick").css("color","#0000ff");
-    $(".rightRateClick").css("color","#000000");
-    $(".totalTaskClick").css("color","#000000");
+    $('.todayTaskClick').css('color','#0000ff');
+    $('.rightRateClick').css('color','#000000');
+    $('.totalTaskClick').css('color','#000000');
   };
 
   window.rightRateClick = function() {
-    $(".rightRate").show();
-    $(".todayTask").hide();
-    $(".totalTask").hide();
+    $('.rightRate').show();
+    $('.todayTask').hide();
+    $('.totalTask').hide();
 
-    $(".todayTaskClick").css("color","#000000");
-    $(".rightRateClick").css("color","#0000ff");
-    $(".totalTaskClick").css("color","#000000");
+    $('.todayTaskClick').css('color','#000000');
+    $('.rightRateClick').css('color','#0000ff');
+    $('.totalTaskClick').css('color','#000000');
   };
 
   window.totalTaskClick = function() {
-    $(".rightRate").hide();
-    $(".todayTask").hide();
-    $(".totalTask").show();
+    $('.rightRate').hide();
+    $('.todayTask').hide();
+    $('.totalTask').show();
 
-    $(".todayTaskClick").css("color","#000000");
-    $(".rightRateClick").css("color","#000000");
-    $(".totalTaskClick").css("color","#0000ff");
+    $('.todayTaskClick').css('color','#000000');
+    $('.rightRateClick').css('color','#000000');
+    $('.totalTaskClick').css('color','#0000ff');
   };
 
 })
